665b7f414113da56b6a25342cbe7810385251236
Your Name <you@example.com>
2024-03-21 14:15:48 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,7 +45,7 @@ List* crea_lista() {
    List* L = create_list();
    //int arr[10] = {1,2,3,4,5,6,7,8,9,10};
    int *num;
-   for(int i = 0; i < 10; i++)
+   for(int i = 1; i <= 10; i++)
      {
        num = (int*)malloc(sizeof(int));
        *num = i;
